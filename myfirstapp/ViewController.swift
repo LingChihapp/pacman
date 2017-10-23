640a7bca66727645d1852380b982d56875f90803
BESTLab <bestlab@BESTLabde-MacBook-Air.local>
2017-10-23 19:31:08 +0800

    Initial Commit

---
+++ myfirstapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myfirstapp
+//
+//  Created by BESTLab on 2017/10/23.
+//  Copyright © 2017年 BESTLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
